4b2107b50814f66d324b1d8a331728bbb0132193
Krati Jain <72783120+iamkrati22@users.noreply.github.com>
2022-05-30 21:10:36 +0530

    Minor Updation

--- app/src/main/java/com/example/weather_application/MainActivity.java
+++ app/src/main/java/com/example/weather_application/MainActivity.java
@@ -3,12 +3,19 @@ package com.example.weather_application;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText etCity, etCountry;
+    TextView tvResult;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void getWeatherDetails(View view) {
     }
 }
