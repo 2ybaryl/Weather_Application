ee5ae765d6ae772294a984a44ee786945f3c2b17
Naman Khattar <khattarnaman915@gmail.com>
2022-05-31 20:55:25 +0530

    Refined UI, added API Keys

--- app/src/main/java/com/example/weather_application/MainActivity.java
+++ app/src/main/java/com/example/weather_application/MainActivity.java
@@ -7,13 +7,20 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.text.DecimalFormat;
+
 public class MainActivity extends AppCompatActivity {
-    EditText etCity, etCountry;
+    EditText etCity;
     TextView tvResult;
+    private final String url="http://api.weatherapi.com/v1/current.json";
+    private final String apikey="d8d7c444454f48bdadd134514223105";
+    DecimalFormat df= new DecimalFormat("#.##");
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        etCity=findViewById(R.id.etCity);
+        tvResult=findViewById(R.id.tvResult);
     }
 
     public void getWeatherDetails(View view) {
