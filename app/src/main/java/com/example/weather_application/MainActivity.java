e9f9b81a4b691aa5d273f8ae847b6bf3b49f5da6
Naman Khattar <khattarnaman915@gmail.com>
2022-05-31 21:11:19 +0530

    Minor internal changes

--- app/src/main/java/com/example/weather_application/MainActivity.java
+++ app/src/main/java/com/example/weather_application/MainActivity.java
@@ -12,8 +12,7 @@ import java.text.DecimalFormat;
 public class MainActivity extends AppCompatActivity {
     EditText etCity;
     TextView tvResult;
-    private final String url="http://api.weatherapi.com/v1/current.json";
-    private final String apikey="d8d7c444454f48bdadd134514223105";
+    private final String url="http://api.weatherapi.com/v1/current.json?key=d8d7c444454f48bdadd134514223105&q=";
     DecimalFormat df= new DecimalFormat("#.##");
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,5 +23,13 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void getWeatherDetails(View view) {
+        String requrl="";
+        String city=etCity.getText().toString().trim();
+        if(city.equals("")){
+            tvResult.setText("City field should not be empty");
+        } else {
+            requrl=url+city+"&aqi=no";
+        }
+
     }
 }
